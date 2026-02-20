c881e8ff20f29ca7e436d37141f0aa69244415a9
Gokulnathjanakiraman <132862907+Gokulnathjanakiraman@users.noreply.github.com>
2026-02-20 10:37:58 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,41 @@
+package com.erapp23.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    private String empId;
+
+    private String empName;
+    private String department;
+    private String designation;
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+    // getters and setters
+}
